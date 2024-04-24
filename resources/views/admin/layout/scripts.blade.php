2781fdea711a4c5349ab6986b57f44cccfb657f1
CalvinCandra <willycandra850@gmail.com>
2024-04-24 09:21:42 +0800

    Dashboard Desain

---
+++ resources/views/admin/layout/scripts.blade.php
@@ -0,0 +1,3 @@
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+<script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+<!-- <script src="https://kit.fontawesome.com/7eaa0f0932.js" crossorigin="anonymous"></script> -->
---
+++ resources/views/petugas/layout/sidebar.blade.php
@@ -0,0 +1,45 @@
+    <!-- Sidebar -->
+    <aside class="fixed top-0 left-0 z-40 w-64 h-screen pt-14 transition-transform -translate-x-full bg-white border-r border-gray-200 md:translate-x-0" aria-label="Sidenav" id="drawer-navigation">
+      <div class="overflow-y-auto py-5 px-3 h-full bg-white">
+        <ul class="space-y-2">
+          <li>
+            <a href="" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-l hover:bg-gray-100">
+              <i class="fa-solid fa-house text-xl text-black"></i>
+              <span class="ml-3">Dashboard</span>
+            </a>
+          </li>
+          <li>
+            <button
+              type="button"
+              class="flex items-center p-2 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100"
+              aria-controls="dropdown-pages"
+              data-collapse-toggle="dropdown-pages"
+            >
+              <i class="fa-solid fa-user text-xl text-black"></i>
+              <span class="flex-1 ml-4 text-left whitespace-nowrap">User</span>
+              <i class="fa-solid fa-caret-down"></i>
+            </button>
+            <ul id="dropdown-pages" class="hidden py-2 space-y-2">
+              <li>
+                <a
+                  href="#"
+                  class="flex items-center p-2 pl-11 w-full text-base font-medium text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100"
+                  >
+                  <i class="fa-solid fa-graduation-cap me-2"></i>
+                  Data Peserta
+                  </a
+                >
+              </li>
+            </ul>
+          </li>
+
+          <li>
+            <a href="" class="flex items-center p-2 text-base font-medium text-gray-900 rounded-l hover:bg-gray-100">
+              <i class="fa-solid fa-book text-xl text-black"></i>
+              <span class="ml-4">Soal</span>
+            </a>
+          </li>
+          
+        </ul>
+      </div>
+    </aside>
